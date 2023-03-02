3678dff61f044f9bfbe4442b92ae0d42391f662d
Robert Stupp <snazy@snazy.de>
2023-03-02 10:32:06 +0100

    Refactor Maven group-id to `org.projectnessie.nessie-runner` (#161)
    Part of an overall effort to clean-up the use of the Maven group-id `org.projectnessie`, which is used by several distinct projects.
    
    * Maven plugin moved from `org.projectnessie:nessie-apprunner-maven-plugin` to `org.projectnessie.nessie-runner:nessie-runner-maven-plugin`, added a relocation Maven project for the Maven plugin
    * Renamed directories in this source repo
    * Moved classes to base package `org.projectnessie.nessierunner` with sub-packages `common`, `maven`, `gradle` for the three sub-projects

---
+++ gradle-plugin/src/main/java/org/projectnessie/nessierunner/gradle/NessieRunnerExtension.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.nessierunner.gradle;
+
+import java.io.File;
+import java.util.Collections;
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.tasks.TaskCollection;
+import org.gradle.api.tasks.TaskProvider;
+
+public class NessieRunnerExtension {
+  private final MapProperty<String, String> environment;
+  private final MapProperty<String, String> environmentNonInput;
+  private final MapProperty<String, String> systemProperties;
+  private final MapProperty<String, String> systemPropertiesNonInput;
+  private final ListProperty<String> arguments;
+  private final ListProperty<String> argumentsNonInput;
+  private final ListProperty<String> jvmArguments;
+  private final ListProperty<String> jvmArgumentsNonInput;
+  private final Property<Integer> javaVersion;
+  private final Property<String> httpListenPortProperty;
+  private final Property<String> httpListenUrlProperty;
+  private final RegularFileProperty executableJar;
+  private final RegularFileProperty workingDirectory;
+  private final Property<Long> timeToListenUrlMillis;
+  private final Property<Long> timeToStopMillis;
+
+  private final Provider<NessieRunnerService> nessieRunnerServiceProvider;
+
+  public NessieRunnerExtension(
+      Project project, Provider<NessieRunnerService> nessieRunnerServiceProvider) {
+    this.nessieRunnerServiceProvider = nessieRunnerServiceProvider;
+
+    environment = project.getObjects().mapProperty(String.class, String.class);
+    environmentNonInput = project.getObjects().mapProperty(String.class, String.class);
+    systemProperties =
+        project
+            .getObjects()
+            .mapProperty(String.class, String.class)
+            .convention(Collections.singletonMap("quarkus.http.port", "0"));
+    systemPropertiesNonInput = project.getObjects().mapProperty(String.class, String.class);
+    arguments = project.getObjects().listProperty(String.class);
+    argumentsNonInput = project.getObjects().listProperty(String.class);
+    jvmArguments = project.getObjects().listProperty(String.class);
+    jvmArgumentsNonInput = project.getObjects().listProperty(String.class);
+    javaVersion = project.getObjects().property(Integer.class).convention(11);
+    httpListenUrlProperty =
+        project.getObjects().property(String.class).convention("quarkus.http.test-url");
+    httpListenPortProperty =
+        project.getObjects().property(String.class).convention("quarkus.http.test-port");
+    workingDirectory =
+        project
+            .getObjects()
+            .fileProperty()
+            .convention(() -> new File(project.getBuildDir(), "nessie-quarkus"));
+    executableJar = project.getObjects().fileProperty();
+    timeToListenUrlMillis = project.getObjects().property(Long.class).convention(0L);
+    timeToStopMillis = project.getObjects().property(Long.class).convention(0L);
+  }
+
+  public MapProperty<String, String> getSystemProperties() {
+    return systemProperties;
+  }
+
+  public MapProperty<String, String> getSystemPropertiesNonInput() {
+    return systemPropertiesNonInput;
+  }
+
+  public MapProperty<String, String> getEnvironment() {
+    return environment;
+  }
+
+  public MapProperty<String, String> getEnvironmentNonInput() {
+    return environmentNonInput;
+  }
+
+  public ListProperty<String> getArguments() {
+    return arguments;
+  }
+
+  public ListProperty<String> getArgumentsNonInput() {
+    return argumentsNonInput;
+  }
+
+  public ListProperty<String> getJvmArguments() {
+    return jvmArguments;
+  }
+
+  public ListProperty<String> getJvmArgumentsNonInput() {
+    return jvmArgumentsNonInput;
+  }
+
+  public Property<Integer> getJavaVersion() {
+    return javaVersion;
+  }
+
+  public Property<String> getHttpListenPortProperty() {
+    return httpListenPortProperty;
+  }
+
+  public Property<String> getHttpListenUrlProperty() {
+    return httpListenUrlProperty;
+  }
+
+  public RegularFileProperty getExecutableJar() {
+    return executableJar;
+  }
+
+  public RegularFileProperty getWorkingDirectory() {
+    return workingDirectory;
+  }
+
+  public Property<Long> getTimeToListenUrlMillis() {
+    return timeToListenUrlMillis;
+  }
+
+  public Property<Long> getTimeToStopMillis() {
+    return timeToStopMillis;
+  }
+
+  public NessieRunnerExtension includeTasks(TaskCollection<? extends Task> taskCollection) {
+    return includeTasks(taskCollection, null);
+  }
+
+  public <T extends Task> NessieRunnerExtension includeTasks(
+      TaskCollection<T> taskCollection, Action<T> postStartAction) {
+    taskCollection.configureEach(
+        new NessieRunnerTaskConfigurer<>(postStartAction, nessieRunnerServiceProvider));
+    return this;
+  }
+
+  public NessieRunnerExtension includeTask(TaskProvider<? extends Task> taskProvider) {
+    return includeTask(taskProvider, null);
+  }
+
+  public <T extends Task> NessieRunnerExtension includeTask(
+      TaskProvider<T> taskProvider, Action<T> postStartAction) {
+    taskProvider.configure(
+        new NessieRunnerTaskConfigurer<>(postStartAction, nessieRunnerServiceProvider));
+    return this;
+  }
+}
---
+++ gradle-plugin/src/main/java/org/projectnessie/nessierunner/gradle/ProcessState.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.nessierunner.gradle;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+import org.gradle.api.GradleException;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.artifacts.ModuleDependency;
+import org.gradle.api.file.RegularFile;
+import org.gradle.api.plugins.ExtraPropertiesExtension;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.testing.Test;
+import org.gradle.process.JavaForkOptions;
+import org.projectnessie.nessierunner.common.JavaVM;
+import org.projectnessie.nessierunner.common.ProcessHandler;
+import org.slf4j.Logger;
+
+public class ProcessState {
+
+  private ProcessHandler processHandler;
+
+  public ProcessState() {
+    // intentionally empty
+  }
+
+  @TaskAction
+  public void noop() {}
+
+  void quarkusStart(Task task) {
+    NessieRunnerExtension extension =
+        task.getProject().getExtensions().getByType(NessieRunnerExtension.class);
+
+    Configuration appConfig =
+        task.getProject().getConfigurations().getByName(NessieRunnerPlugin.APP_CONFIG_NAME);
+
+    RegularFile configuredJar = extension.getExecutableJar().getOrNull();
+
+    DependencySet dependencies = appConfig.getDependencies();
+    File execJar;
+
+    if (configuredJar == null) {
+      if (dependencies.isEmpty()) {
+        throw new GradleException(
+            String.format(
+                "Dependency org.projectnessie:nessie-quarkus:runner missing in configuration %s",
+                NessieRunnerPlugin.APP_CONFIG_NAME));
+      }
+      if (dependencies.size() != 1) {
+        throw new GradleException(
+            String.format(
+                "Configuration %s must only contain the org.projectnessie:nessie-quarkus:runner dependency, but resolves to these artifacts: %s",
+                NessieRunnerPlugin.APP_CONFIG_NAME,
+                dependencies.stream()
+                    .map(d -> String.format("%s:%s:%s", d.getGroup(), d.getName(), d.getVersion()))
+                    .collect(Collectors.joining(", "))));
+      }
+      Dependency dep = dependencies.iterator().next();
+      if (!(dep instanceof ModuleDependency)) {
+        throw new GradleException(
+            String.format(
+                "Expected dependency %s to be of type ModuleDependency, but is %s",
+                dep, dep.getClass().getSimpleName()));
+      }
+      Set<File> files = appConfig.resolve();
+      if (files.size() != 1) {
+        throw new GradleException(
+            String.format(
+                "Expected configuration %s with dependency %s to resolve to exactly one artifact, but resolves to %s (hint: do not enable transitive on the dependency)",
+                NessieRunnerPlugin.APP_CONFIG_NAME, dep, files));
+      }
+      execJar = files.iterator().next();
+    } else {
+      if (!dependencies.isEmpty()) {
+        throw new GradleException(
+            String.format(
+                "Configuration %s contains a dependency and option 'executableJar' are mutually exclusive",
+                NessieRunnerPlugin.APP_CONFIG_NAME));
+      }
+      execJar = configuredJar.getAsFile();
+    }
+
+    JavaVM javaVM = JavaVM.findJavaVM(extension.getJavaVersion().get());
+    if (javaVM == null) {
+      throw new GradleException(noJavaMessage(extension.getJavaVersion().get()));
+    }
+
+    Path workDir = extension.getWorkingDirectory().getAsFile().get().toPath();
+    if (!Files.isDirectory(workDir)) {
+      try {
+        Files.createDirectories(workDir);
+      } catch (IOException e) {
+        throw new GradleException(
+            String.format("Failed to create working directory %s", workDir), e);
+      }
+    }
+
+    List<String> command = new ArrayList<>();
+    command.add(javaVM.getJavaExecutable().toString());
+    command.addAll(extension.getJvmArguments().get());
+    command.addAll(extension.getJvmArgumentsNonInput().get());
+    extension
+        .getSystemProperties()
+        .get()
+        .forEach((k, v) -> command.add(String.format("-D%s=%s", k, v)));
+    extension
+        .getSystemPropertiesNonInput()
+        .get()
+        .forEach((k, v) -> command.add(String.format("-D%s=%s", k, v)));
+    command.add("-Dquarkus.http.port=0");
+    command.add("-jar");
+    command.add(execJar.getAbsolutePath());
+    command.addAll(extension.getArguments().get());
+    command.addAll(extension.getArgumentsNonInput().get());
+
+    task.getLogger().info("Starting process: {}", command);
+
+    ProcessBuilder processBuilder = new ProcessBuilder().command(command);
+    extension.getEnvironment().get().forEach((k, v) -> processBuilder.environment().put(k, v));
+    extension
+        .getEnvironmentNonInput()
+        .get()
+        .forEach((k, v) -> processBuilder.environment().put(k, v));
+    processBuilder.directory(workDir.toFile());
+
+    try {
+      processHandler = new ProcessHandler();
+      processHandler.setStdoutTarget(line -> task.getLogger().info("[stdout] {}", line));
+      processHandler.setStderrTarget(line -> task.getLogger().info("[stderr] {}", line));
+      processHandler.start(processBuilder);
+      if (extension.getTimeToListenUrlMillis().get() > 0L) {
+        processHandler.setTimeToListenUrlMillis(extension.getTimeToListenUrlMillis().get());
+      }
+      if (extension.getTimeToStopMillis().get() > 0L) {
+        processHandler.setTimeStopMillis(extension.getTimeToStopMillis().get());
+      }
+      processHandler.getListenUrl();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new GradleException(String.format("Process-start interrupted: %s", command), e);
+    } catch (TimeoutException e) {
+      throw new GradleException(
+          String.format("Nessie-Server/Quarkus did not emit listen URL. Process: %s", command), e);
+    } catch (IOException e) {
+      throw new GradleException(String.format("Failed to start the process %s", command), e);
+    }
+
+    String listenUrl;
+    try {
+      listenUrl = processHandler.getListenUrl();
+    } catch (Exception e) {
+      // Can safely ignore it (it invocation does not block and therefore not throw an exception).
+      // But make the IDE happy with this throw.
+      throw new RuntimeException(e);
+    }
+    String listenPort = Integer.toString(URI.create(listenUrl).getPort());
+
+    // Add the Quarkus properties as "generic properties", so any task can use them.
+    ExtraPropertiesExtension extra = task.getExtensions().getByType(ExtraPropertiesExtension.class);
+    extra.set(extension.getHttpListenUrlProperty().get(), listenUrl);
+    extra.set(extension.getHttpListenPortProperty().get(), listenPort);
+
+    // Do not put the "dynamic" properties (quarkus.http.test-port) to the `Test` task's
+    // system-properties, because those are subject to the test-task's inputs, which is used
+    // as the build-cache key. Instead, pass the dynamic properties via a
+    // CommandLineArgumentProvider.
+    // In other words: ensure that the `Test` tasks is cacheable.
+    if (task instanceof JavaForkOptions) {
+      Test test = (Test) task;
+      test.getJvmArgumentProviders()
+          .add(
+              () ->
+                  Arrays.asList(
+                      String.format(
+                          "-D%s=%s", extension.getHttpListenUrlProperty().get(), listenUrl),
+                      String.format(
+                          "-D%s=%s", extension.getHttpListenPortProperty().get(), listenPort)));
+    }
+  }
+
+  void quarkusStop(Logger logger) {
+    if (processHandler == null) {
+      logger.debug("No application found.");
+      return;
+    }
+
+    try {
+      processHandler.stop();
+      logger.info("Quarkus application stopped.");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      processHandler = null;
+    }
+  }
+
+  static String noJavaMessage(int version) {
+    return String.format(
+        "Could not find a Java-VM for Java version %d. "
+            + "Set the Java-Home for a compatible JVM using the environment variable JDK%d_HOME or "
+            + "JAVA%d_HOME.",
+        version, version, version);
+  }
+}
